ffac435c572bcdb18bdfdcf2d8b05e5035f12ee8
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-01 13:15:04 -0700

    Implement a first pass answer for stock_prices.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,27 @@
 
 import argparse
 
+
+# First Pass
 def find_max_profit(prices):
-  pass
+    difference_list = []
+    for i in range(1, len(prices)):
+        j = i + 1
+        while j < len(prices):
+            diff = prices[j] - prices[i]
+            difference_list.append(diff)
+            j += 1
+    return max(difference_list)
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
+
+# print(find_max_profit([1050, 270, 1540, 3800, 2]))
+# print(find_max_profit([100, 90, 80, 50, 20, 10]))
