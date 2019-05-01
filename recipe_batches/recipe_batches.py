e462ddd2bffe4af0e4b63c35c11dcdf08e9f3002
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-01 14:08:25 -0700

    Implement a first pass working answer for recipe_batches.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,28 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
+
+# First Pass
+def recipe_batch(recipe, ingredients):
+    max_batches = -1
+    for r in recipe:
+        if r not in ingredients:
+            return 0
+        batches = (ingredients[r] // recipe[r])
+        if max_batches == -1:
+            max_batches = batches
+        elif batches < max_batches:
+            max_batches = batches
+    return max_batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batch(recipe, ingredients), ingredients=ingredients))
+
+
+# print(recipe_batch({'milk': 100, 'butter': 50, 'flour': 5}, {'milk': 132, 'butter': 48, 'flour': 51}))
