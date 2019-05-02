b410281f1b3e8c96239967c9e1dba3f131e6662a
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-01 17:43:15 -0700

    Implemented eating_cookies.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,25 +7,34 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-    if n == 1:
-        return 1
-    if n < 1:
+    if n < 0:
         return 0
-    counter = 0
-    for i in range(1, n + 1):
-        for j in range(1, 4):
-            counter += eating_cookies(n-j)
-    print(counter)
-    return counter
+    elif n <= 1:
+        return 1
+    return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
 
 
-# if __name__ == "__main__":
-#     if len(sys.argv) > 1:
-#         num_cookies = int(sys.argv[1])
-#         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
-#                                                                                     n=num_cookies))
-#     else:
-#         print('Usage: eating_cookies.py [num_cookies]')
+def eating_cookies_alt(n, cache=None):
+    if n < 0:
+        return 0
+    elif n <= 1:
+        return 1
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n + 1)}
+        cache[n] = eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
+        return cache[n]
 
-print(eating_cookies(10))
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
+                                                                                    n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
+
+# print(eating_cookies(10))
+# print(eating_cookies_alt(50))
